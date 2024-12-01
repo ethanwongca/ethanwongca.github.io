434034b4b5bbc3afd5f30f48594877d0b80ef4a3
Ethan Wong <ethanwongca@gmail.com>
2024-12-01 13:28:16 -0800

    Updated the CV and About section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,13 +10,14 @@ const About: React.FC = () => {
             Ethan Parker Wong
           </h1>
           <p className="text-xl mb-2">
-            <a href="ethanwongca.github.io" className="text-pink-600">Computer Science at UBC. </a> Vancouver, BC. <a href="https://www.linkedin.com/in/ethanwongca/" className="text-pink-600 hover:underline">Linkedin</a>.
+            Vancouver, BC. <a href="https://www.linkedin.com/in/ethanwongca/" className="text-pink-600 hover:underline">Linkedin</a>.
           </p>
           <p className="text-lg mb-4">
-            Hello, my name is Ethan and I am Computer Science Student at UBC. I am currently developing artificial intelligence tools for the <a href="https://www.linkedin.com/in/ethanwongca/" className="text-pink-500"> United Nations </a> and UBC. I am interested in software development, human computer interaction, and machine learning. On this portfolio you can find my resume, projects, and publications.
+            Hello! My name is Ethan and I am a Computer Science Student at UBC. I am an incoming business intelligence engineer intern at<a href="https://www.linkedin.com/in/ethanwongca/" className="text-pink-500">  Amazon </a> and have developed artificial intelligence tools for the <a href="https://www.linkedin.com/in/ethanwongca/" className="text-pink-500">  United Nations. </a>
+            I am currently doing deep learning research at UBC's Natural Language Processing Group and Human Artificial Intelligence Lab. My research interests are in natural language processing, human-computer interaction, and human-centered computing. On this portfolio, you can find my resume, projects, and publications.
           </p>
           <p className="text-lg mb-4">
-            Besides computer science, I love being active. I have ran three half-marathons, have a black belt in Taekwondo, and I am preparing for my first century bike ride. I also love to travel and am always looking for new places to explore. 
+            Besides computer science, I love being active. I have run four half-marathons, have a black belt in Taekwondo, and I am preparing for my first century bike ride. I also love to travel and am always looking for new places to explore. 
           </p>
           <p className="text-lg mb-4">
             Feel free to reach out to me on my <a href="mailto:ethanwongca@gmail.com" className="text-pink-600">email</a>, ethanwongca@gmail.com if any of my work interests you. I am always looking for new opportunities to learn and grow!
--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -20,34 +20,38 @@ const Experience: React.FC = () => {
         {/* Main Content */}
         <div className="w-full lg:w-3/4 p-4 space-y-8">
           <div id="basics" className="bg-white shadow rounded-lg p-6">
-            <h2 className="text-2xl font-bold mb-4">Basics</h2>
+            <h2 className="text-2xl font-bold mb-4">Information</h2>
             <p><strong>Name:</strong> Ethan Parker Wong</p>
-            <p><strong>Label:</strong> Computer Science Student</p>
             <p><strong>Email:</strong> <a href="mailto:ethanwongca@gmail.com" className="text-pink-600">ethanwongca@gmail.com</a></p>
-            <p><strong>Phone:</strong> (236) 889-3873</p>
-            <p><strong>URL:</strong> <a href="https://ethanwongca.github.io/" className="text-pink-600">https://ethanwongca.github.io/</a></p>
-            <p><strong>Summary:</strong> A computer science student interested in machine learning and human computer interaction.</p>
+            <p><strong>Phone:</strong> +1 (236) 889-3873</p>
+            <p><strong>URL:</strong> <a href="https://ethanwongca.github.io/" className="text-pink-600">https://ethanwong.ca/</a></p>
+            <p><strong>Summary:</strong>  A computer science student with incoming FAANG experience and an interest in human-centered computing. </p>
           </div>
 
           <div id="experience" className="bg-white shadow rounded-lg p-6">
             <h2 className="text-2xl font-bold mb-4">Experience</h2>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
-              <p><strong>Company:</strong> University of British Columbia NLP Group</p>
+              <p><strong>Company:</strong> Amazon</p>
+              <p><strong>Role:</strong> Incoming Business Intelligence Engineer Intern</p>
+              <p><strong>Duration:</strong> Incoming May 2025 </p>
+            </div>
+            <div className="border-l-4 border-gray-300 pl-4 mb-4">
+              <p><strong>Company:</strong> University of British Columbia Natural Language Processing Group</p>
               <p><strong>Role:</strong> Research Assistant</p>
               <p><strong>Duration:</strong> August 2024 - Present</p>
-              <p><strong>Responsibilities:</strong> Writing Python scripts for web scraping and developing NLP models.</p>
+              <p><strong>Responsibilities:</strong> Supervision under Professor Jian Zhu and funding via UBC Work Learn. Working on NLP and Computational Social Sciences Research. </p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
               <p><strong>Company:</strong> United Nations</p>
               <p><strong>Role:</strong> Artificial Intelligence Fellow</p>
-              <p><strong>Duration:</strong> May 2024 - Present</p>
-              <p><strong>Responsibilities:</strong> Currently developing AI tools for the UN-OICT Innovation Unit.</p>
+              <p><strong>Duration:</strong> May 2024 - August 2024</p>
+              <p><strong>Responsibilities:</strong> Developed artificial intelligence tools and evaluated open-source generative artificial intelligence at the UN-OICT Innovation Unit. </p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
               <p><strong>Company:</strong> University of British Columbia</p>
               <p><strong>Role:</strong> Research Assistant</p>
               <p><strong>Duration:</strong> March 2022 - April 2024</p>
-              <p><strong>Responsibilities:</strong> Did data visualization and automated excel files for a hepatic blood flow literature review. Currently doing AI and epidemiology.</p>
+              <p><strong>Responsibilities:</strong> Supervised by Ph.D. Candidate Lisa Cheng. Worked on a hepatic blood flow literature review. </p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
               <p><strong>Company:</strong> McGill University</p>
@@ -59,13 +63,13 @@ const Experience: React.FC = () => {
               <p><strong>Company:</strong> Google</p>
               <p><strong>Role:</strong> Computer Science Research Mentorship Scholar</p>
               <p><strong>Duration:</strong> September 2023 - December 2023 </p>
-              <p><strong>Responsibilities:</strong> Selected in a three month program to be mentored by a Google Employee in HCI and NLP.</p>
+              <p><strong>Responsibilities:</strong> Selected in a three-month program to be mentored by a Google Employee in HCI and NLP.</p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
               <p><strong>Company:</strong> APOLLO Insurance</p>
               <p><strong>Role:</strong> Agency Intern</p>
               <p><strong>Duration:</strong> June 2023 - August 2023 </p>
-              <p><strong>Responsibilities:</strong> Managed an insurance database with SQL. </p>
+              <p><strong>Responsibilities:</strong> Managed an insurance database. </p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
               <p><strong>Company:</strong> Koi Continuity</p>
@@ -122,27 +126,27 @@ const Experience: React.FC = () => {
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
                 <p><strong>Award:</strong> Tomlinson Engagement Award for Mentoring</p>
                 <p><strong>Year:</strong> 2024</p>
-                <p><strong>Details:</strong> Mentoring students at McGill</p>
+                <p><strong>Details:</strong> Mentoring students in computer science at McGill.</p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
                 <p><strong>Award:</strong> Governor General's Academic Bronze Medal</p>
                 <p><strong>Year:</strong> 2022</p>
-                <p><strong>Details:</strong> Rank 1 at Magee Secondary School</p>
+                <p><strong>Details:</strong> Rank 1 out of 216 at Magee Secondary School</p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
                 <p><strong>Award:</strong> Alma Mater Scholarship</p>
                 <p><strong>Year:</strong> 2022</p>
-                <p><strong>Details:</strong> McGill Entry Scholarship</p>
+                <p><strong>Details:</strong> McGill Entry Scholarship.</p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
-                <p><strong>Award:</strong> British Columbia Achievement Scholarshp</p>
+                <p><strong>Award:</strong> British Columbia Achievement Scholarship</p>
                 <p><strong>Year:</strong> 2022</p>
-                <p><strong>Details:</strong> Top 8,000 graduates from British Columbia</p>
+                <p><strong>Details:</strong> Top 8,000 graduates from British Columbia.</p>
             </div>
             <div className="border-l-4 border-gray-300 pl-4 mb-4">
                 <p><strong>Award:</strong> Special Mention in UNHCR's MUN Refugee Challenge</p>
                 <p><strong>Year:</strong> 2022</p>
-                <p><strong>Details:</strong> Won for ideas in empowering refugees with technology over 350 of the top international MUNs</p>
+                <p><strong>Details:</strong> Won for ideas in empowering refugees with technology over 350 of the top international MUNs.</p>
             </div>
           </div>
 
@@ -151,14 +155,14 @@ const Experience: React.FC = () => {
             <p><strong>Title:</strong> Analyzing Language Bias Between French and English in Conventional Multilingual Sentiment Analysis Models</p>
             <p><strong>Journal:</strong> arXiv</p>
             <p><strong>Year:</strong> 2024</p>
-            <p><strong>Details:</strong> Wrote a paper on bias in sentiment analysis machine learning models. </p>
+            <p><strong>Details:</strong> Wrote a paper, with two citations, on bias in sentiment analysis machine learning models. </p>
           </div>
 
           <div id="skills" className="bg-white shadow rounded-lg p-6">
             <h2 className="text-2xl font-bold mb-4">Skills</h2>
-            <p><strong>Programming Languages:</strong> Python, JavaScript, R, TypeScript, Bash, C, Java</p>
-            <p><strong>Technologies:</strong> React, Node.js, SQL, Power BI, AWS</p>
-            <p><strong>Others:</strong> Data Analysis, Machine Learning</p>
+            <p><strong>Programming Languages:</strong> Python, Java, C, C++, SQL, TypeScript, JavaScript, HTML, CSS, R, Bash</p>
+            <p><strong>Technologies:</strong> React, Flask, Express, Node.js, Linux, Unix, Pandas, PyTorch, NumPy, Git, Docker, Kubernetes</p>
+            <p><strong>Others:</strong> Full-Stack, Databases, Software Design, Cloud Computing, Data Structures, Algorithms, Software Systems, Agile Methodologies, Web Development, Frontend, Backend, Object-Oriented Design, CI/CD Pipeline, Unit Testing</p>
           </div>
 
         </div>
